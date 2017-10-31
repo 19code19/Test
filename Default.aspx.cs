bc31ee424fe8002d6c5cd4f2fddbf27a46b61f42
Biswajit Senapati <biswajit.s@BRILLIO.COM>
2017-10-31 17:55:03 +0530

    Signed-off-by: Biswajit Senapati <biswajit.s@BRILLIO.COM> Final changes

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -9,7 +9,7 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        Response.Write("Hellow");
-        Response.Write("Testing 2nd time");
+        Response.Write("Final Testing");
+        
     }
 }
